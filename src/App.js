b042c794290e74430e8a6d22458d200f6c328858
Larrea <gorkalarreamontoya@opendeusto.es>
2025-06-03 10:59:13 +0200

    Updates que lo rompen todo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Switch, Route, Redirect } from "react-router";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { Switch, Route, Redirect, HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { ConnectedRouter } from "connected-react-router";
-import { getHistory } from "./index";
 import { AdminRoute, UserRoute, AuthRoute } from "./RouteComponents";
 
 import ErrorPage from "./pages/error/ErrorPage.js";
@@ -19,32 +16,30 @@ const App = (props) => {
   return (
     <div>
       <ToastContainer/>
-      <ConnectedRouter history={getHistory()}>
-        <HashRouter>
-          <Switch>
-            <Route path="/" exact render={() => <Redirect to="/template/dashboard" />} />
-            <Route path="/template" exact render={() => <Redirect to="/template/dashboard"/>} />
-            <UserRoute
-              path="/template"
-              dispatch={props.dispatch}
-              component={LayoutComponent}
-            />
-            <AdminRoute
-              path="/admin"
-              currentUser={props.currentUser}
-              dispatch={props.dispatch}
-              component={LayoutComponent}
-            />
-            <Route path="/documentation" exact render={() => <Redirect to="/documentation/getting-started/overview"/>}/>
-            <Route path="/documentation" component={DocumentationLayout}/>
-            <AuthRoute path="/login" exact component={Login} />
-            <AuthRoute path="/register" exact component={Register} />
-            <Route path="/error" exact component={ErrorPage} />
-            <Redirect from="*" to="/template/dashboard" />
-            <Route path='*' exact render={() => <Redirect to="/error" />} />
-          </Switch>
-        </HashRouter>
-      </ConnectedRouter>
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact render={() => <Redirect to="/template/dashboard" />} />
+          <Route path="/template" exact render={() => <Redirect to="/template/dashboard"/>} />
+          <UserRoute
+            path="/template"
+            dispatch={props.dispatch}
+            component={LayoutComponent}
+          />
+          <AdminRoute
+            path="/admin"
+            currentUser={props.currentUser}
+            dispatch={props.dispatch}
+            component={LayoutComponent}
+          />
+          <Route path="/documentation" exact render={() => <Redirect to="/documentation/getting-started/overview"/>}/>
+          <Route path="/documentation" component={DocumentationLayout}/>
+          <AuthRoute path="/login" exact component={Login} />
+          <AuthRoute path="/register" exact component={Register} />
+          <Route path="/error" exact component={ErrorPage} />
+          <Redirect from="*" to="/template/dashboard" />
+          <Route path='*' exact render={() => <Redirect to="/error" />} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
